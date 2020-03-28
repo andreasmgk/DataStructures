787dae2679fcc5c250275bbfe0d2b24532d1106d
andreasmgk <andreasmg@csd.auth.gr>
2020-03-28 19:26:40 +0200

    Added  BinaryTree2

---
+++ BinaryTree2.cpp
@@ -0,0 +1,11 @@
+#include "BinaryTree2.h"
+
+BinaryTree2::BinaryTree2()
+{
+    //ctor
+}
+
+BinaryTree2::~BinaryTree2()
+{
+    //dtor
+}
---
+++ BinaryTree2.h
@@ -0,0 +1,16 @@
+#ifndef BINARYTREE2_H
+#define BINARYTREE2_H
+
+
+class BinaryTree2
+{
+    public:
+        BinaryTree2();
+        ~BinaryTree2();
+
+    protected:
+
+    private:
+};
+
+#endif // BINARYTREE2_H
