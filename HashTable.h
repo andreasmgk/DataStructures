6f0b16b1cf298edd4766eb9cfeae756e42e38f32
Giorgos Karapiperidis <georgekarapi@yahoo.gr>
2020-05-22 09:29:42 +0300

    Finished HashTable

---
+++ HashTable.h
@@ -0,0 +1,25 @@
+#ifndef DATASTRUCTURES_HASHTABLE_H
+#define DATASTRUCTURES_HASHTABLE_H
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Cell {
+    int count;
+    string word;
+};
+
+class HashTable
+{
+private:
+    Cell *table;
+    Cell *growTable();
+    int tab_len;
+public:
+    HashTable();
+    void insert(const string&);
+    Cell *search(const string&);
+    void print(const string&);
+};
+#endif //DATASTRUCTURES_HASHTABLE_H
