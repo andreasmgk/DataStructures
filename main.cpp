cc3a37fefad0c7645bb6b0ac2a65c106b1b14a6e
andreasmgk <andreasmg@csd.auth.gr>
2020-05-23 18:11:59 +0300

    HashTable

--- main.cpp
+++ main.cpp
@@ -5,24 +5,30 @@
 
 using namespace std;
 
-int main(){    
+int main(){
+
+    string h[5] = {"d", "a", "b", "c", "e"};
+
     //BinaryTree b;
     BinaryTree a;
 
     cout<< "BinaryTree" <<endl;
     cout<< "------------------------------" <<endl;
-    a.insert("hello");
-    a.insert("cat");
-    a.insert("apple");
-    a.insert("apple");
-    a.insert("apple");
+    for(int i = 0; i<1000000;i++)
+        a.insert(h[i % 5]);
+    a.preorder();
+    cout<<endl;
     a.inorder();
     cout<<endl;
-    a.deletek("apple");
+    a.postorder();
+    cout<<endl;
+    for(int i = 0; i<1000000; i++)
+        a.deletek(h[i % 4]);
+    a.preorder();
+    cout<<endl;
     a.inorder();
     cout<<endl;
-    a.deletek("apple");
-    a.inorder();
+    a.postorder();
     cout<<endl;
 
     //AVLTree
@@ -30,18 +36,21 @@ int main(){
 
     cout<< "AVLTree" <<endl;
     cout<< "------------------------------" <<endl;
-    b.insert("hello");
-    b.insert("cat");
-    b.insert("apple");
-    b.insert("apple");
-    b.insert("apple");
+    for(int i = 0; i<1000000;i++)
+        b.insert(h[i % 5]);
+    b.preorder();
+    cout<<endl;
     b.inorder();
     cout<<endl;
-    b.deletek("apple");
+    b.postorder();
+    cout<<endl;
+    for(int i = 0; i<1000000; i++)
+        b.deletek(h[i % 1]);
+    b.preorder();
+    cout<<endl;
     b.inorder();
     cout<<endl;
-    b.deletek("apple");
-    b.inorder();
+    b.postorder();
     cout<<endl;
 
     //AVLTree
@@ -49,15 +58,13 @@ int main(){
 
     cout<< "HashTable" <<endl;
     cout<< "------------------------------" <<endl;
-    c.insert("hello");
-    c.insert("cat");
-    c.insert("apple");
-    c.insert("apple");
-    c.insert("apple");
-    c.insert("apple");
-    c.print("apple");
-    c.print("cat");
-    c.print("hello");
+    for(int i = 0; i<1000000000;i++)
+        c.insert(h[i % 5]);
+    c.print("a");
+    c.print("b");
+    c.print("c");
+    c.print("d");
+    c.print("e");
     cout<<endl;
     return 0;
 }
